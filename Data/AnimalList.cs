29f3c0ad4233b3d810b61efb684cbce2f1a85564
Jean Bryant Figueroa <jbster559@gmail.com>
2025-04-23 21:32:14 -0700

    Created static list

---
+++ Data/AnimalList.cs
@@ -0,0 +1,32 @@
+﻿using COMP003B.Assignment5.Models;
+
+namespace COMP003B.Assignment5.Data
+{
+    public class AnimalList
+    {
+        public static List<ZooAnimal> Animals { get; } = new List<ZooAnimal>
+        {
+            new ZooAnimal
+            {
+                Id = 1,
+                Name = "Rosa",
+                Species = "Sea Otter",
+                Age = 15
+            },
+            new ZooAnimal
+            {
+                Id = 2,
+                Name = "Tony",
+                Species = "Tiger",
+                Age = 8
+            },
+            new ZooAnimal
+            {
+                Id = 3,
+                Name = "Rasmus",
+                Species = "Owl",
+                Age = 4
+            }
+        };
+    }
+}
