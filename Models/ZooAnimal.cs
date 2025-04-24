3fa31e9b20376d2ea25ba02ed2991f33d37b73cb
Jean Bryant Figueroa <jbster559@gmail.com>
2025-04-23 21:24:33 -0700

    Add project files.

---
+++ Models/ZooAnimal.cs
@@ -0,0 +1,11 @@
+﻿namespace COMP003B.Assignment5.Models
+{
+    public class ZooAnimal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Age { get; set; }
+
+    }
+}
